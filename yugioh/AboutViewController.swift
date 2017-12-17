94e3dcd886c1f11f5273eef371c41042961ba3c5
Aaron-zheng <fucheng.zheng@gmail.com>
2017-12-18 00:35:24 +0800

    添加了注册和登录的界面和功能

---
+++ yugioh/AboutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.swift
+//  游戏王卡牌
+//
+//  Created by Aaron on 18/12/2017.
+//  Copyright © 2017 sightcorner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    
+}
---
+++ yugioh/SignupViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SignupViewController.swift
+//  游戏王卡牌
+//
+//  Created by Aaron on 17/12/2017.
+//  Copyright © 2017 sightcorner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
