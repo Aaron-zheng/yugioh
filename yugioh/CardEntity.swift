fbb849a8cfd851ca572089802453399c8254a2ad
Aaron-zheng <fucheng.zheng@gmail.com>
2018-12-31 19:00:50 +0800

    添加注释

--- yugioh/CardEntity.swift
+++ yugioh/CardEntity.swift
@@ -10,23 +10,56 @@ import Foundation
 
 class CardEntity {
     
-    //和xml数据库对应的属性
+    //唯一标识
     var id: String! = ""
+    
+    //中文名称
     var titleChinese: String! = ""
+    
+    //日文名称
     var titleJapanese: String! = ""
+    
+    //英文名称
     var titleEnglish: String! = ""
+    
+    //类型，空格隔离
     var type: String! = ""
+    
+    //密码，唯一标识
     var password: String! = ""
+    
+    //用处，禁止，限制，准限制，无限制（目前已经弃用）
+    @available(*, deprecated)
     var usage: String! = ""
+    
+    //种族，鸟兽，念动力，等等
     var race: String! = ""
+    
+    //属性，光，暗，风，等灯
     var property: String! = ""
+    
+    //星级
     var star: String! = ""
+    
+    //攻击力
     var attack: String! = ""
+    
+    //防御力
     var defense: String! = ""
+    
+    //珍稀程度
     var rare: String! = ""
+    
+    //效果
     var effect: String! = ""
+    
+    //所属卡包
     var pack: String! = ""
+    
+    //灵摆
     var scale: String! = ""
+    
+    //调整
     var adjust: String! = ""
     
     
